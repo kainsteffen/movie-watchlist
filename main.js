551ccc8aa9e8c047fdd575f9e5dc53494eaebfd4
Softcoregamerz <softcoregamerz@hotmail.com>
2020-05-02 19:11:17 +0200

    Franz 01 Fertig
    title

--- main.js
+++ main.js
@@ -1,28 +1,49 @@
 const port = 3000,
  http = require("http"),
  httpStatus = require("http-status-codes"),
- app = http.createServer();
- app.on("request", (req, res) => {
-  var body = [];
-  req.on("data", (bodyData) => {
-  body.push(bodyData);
-  });
-  req.on("end", () => {
-  body = Buffer.concat(body).toString();
-  console.log(`Request Body Contents: ${body}`);
-  });
-  const getJSONString = obj => {
- return JSON.stringify(obj, null, 2);
-};
-  console.log(`Method: ${getJSONString(req.method)}`);
-  console.log(`URL: ${getJSONString(req.url)}`);
-  console.log(`Headers: ${getJSONString(req.headers)}`);
-  res.writeHead(httpStatus.OK, {
-   "Content-Type": "text/html"
-   });
-   let responseMessage = "<h1>This will show on the screen.</h1>";
-   res.end(responseMessage);
-  });
-  app.listen(port);
-  console.log(`The server has started and is listening on port number:
-  ➥ ${port}`);
+ router = require("./router"),
+ contentTypes = require("./contentTypes"),
+ utils = require("./utils");
+ router.get("/", (req, res) => {
+  res.writeHead(httpStatus.OK, contentTypes.htm);
+  utils.getFile("views/index.html", res);
+ });
+ router.get("/myWatchlist.html", (req, res) => {
+  res.writeHead(httpStatus.OK, contentTypes.html);
+  utils.getFile("views/myWatchlist.html", res);
+ });
+ // router.get("/contact.html", (req, res) => {
+ // res.writeHead(httpStatus.OK, contentTypes.html);
+ // utils.getFile("views/contact.html", res);
+ // });
+ // router.post("/", (req, res) => {
+ // res.writeHead(httpStatus.OK, contentTypes.html);
+ // utils.getFile("views/thanks.html", res);
+ // });
+ router.get("/Witcher.jpg", (req, res) => {
+ res.writeHead(httpStatus.OK, contentTypes.jpg);
+ utils.getFile("public/images/Witcher.jpg", res);
+ });
+ router.get("/LotR.jpg", (req, res) => {
+ res.writeHead(httpStatus.OK, contentTypes.jpg);
+ utils.getFile("public/images/LotR.jpg", res);
+ });
+ router.get("/warcraft.jpg", (req, res) => {
+ res.writeHead(httpStatus.OK, contentTypes.jpg);
+ utils.getFile("public/images/warcraft.jpg", res);
+ });
+ router.get("/watchlist.css", (req, res) => {
+ res.writeHead(httpStatus.OK, contentTypes.css);
+ utils.getFile("public/css/watchlist.css", res);
+ });
+ router.get("/bootstrap.css", (req, res) => {
+ res.writeHead(httpStatus.OK, contentTypes.css);
+ utils.getFile("public/css/bootstrap.css", res);
+ });
+ router.get("/watchlist.js", (req, res) => {
+ res.writeHead(httpStatus.OK, contentTypes.js);
+ utils.getFile("public/js/watchlist.js", res);
+ });
+ http.createServer(router.handle).listen(port);
+ console.log(`The server is listening on
+ ➥ port number: ${port}`);
